1d8a0c4c58e0ffa55943bbec23faf0bfdb312d34
gfsilveira <gfsilveira@gmail.com>
2021-07-29 14:07:09 -0300

    Update produtos_inic.sql

--- produtos_inic.sql
+++ produtos_inic.sql
@@ -3,5 +3,5 @@ ALTER TABLE produtos add column usado boolean default false;
 ALTER TABLE produtos add column descricao varchar(255);
 CREATE TABLE categorias (id integer auto_increment primary key, nome varchar(255));
 insert into categorias (nome) values ("Futebol"), ("Volei"), ("Tenis");
-ALTER TABLE produtos add column catgeoria_id integer;
-update produtos set catgeoria_id = 1;
+ALTER TABLE produtos add column categoria_id integer;
+update produtos set categoria_id = 1;
